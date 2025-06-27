db3579d9f0a11bbf1468154dc9ae15daa0cf104a
azamatxabibullayev <azamatxabibullayev02@gmail.com>
2025-06-27 14:34:50 +0500

    translation front finished

--- dualedu/urls.py
+++ dualedu/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.i18n import set_language
 
 urlpatterns = [
-                  path('ad/', admin.site.urls),
-                  path('', include('main.urls')),
-                  path('users/', include('users.urls')),
-                  path('dashboard/', include('dashboard.urls', namespace='dashboard')),
-                  path('oauth/', include('social_django.urls', namespace='social')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('ad/', admin.site.urls),
+    path('i18n/setlang/', set_language, name='set_language'),
+    path('', include('main.urls')),
+    path('users/', include('users.urls')),
+    path('dashboard/', include(('dashboard.urls', 'dashboard'), namespace='dashboard')),
+    path('oauth/', include('social_django.urls', namespace='social')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
